6b0d9d3c0e70275d94e3462375e2e1a5034231bb
Andrei Vetrov <vettrov78@gmail.com>
2021-12-18 09:38:29 +0300

    Transform to classes of no-parameters views

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { getRandomString } from './utils';
 
 import SiteMenuView from './view/site-menu-view';
 import SiteFilterView from './view/site-filters-view';
+import SiteSortView from './view/site-sort-view';
+import SitePointListView from './view/site-list-view';
 
-import { siteCreateSortTemplate } from './view/site-sort-view';
-import { siteCreatePointListTemplate } from './view/site-list-view';
 import { siteCreatePointTemplate } from './view/site-create-view';
 import { sitePointTemplate } from './view/site-point-view';
 import { renderTemplate, RenderPosition, renderElement } from './render';
@@ -18,12 +18,13 @@ const mainSort = document.querySelector('.trip-events');
 
 // ДОБАВЛЕНИЕ НАВИГАЦИИ И ФИЛЬТРА
 renderElement(controlsNavigation, new SiteMenuView().element, RenderPosition.BEFOREEND);
-renderTemplate(controlsFilters, new SiteFilterView().element, RenderPosition.BEFOREEND);
-renderTemplate(mainSort, siteCreateSortTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(mainSort, siteCreatePointListTemplate(), RenderPosition.BEFOREEND);
+renderElement(controlsFilters, new SiteFilterView().element, RenderPosition.BEFOREEND);
+renderElement(mainSort, new SiteSortView().element, RenderPosition.BEFOREEND);
+renderElement(mainSort, new SitePointListView().element, RenderPosition.BEFOREEND);
 
 const itemsList = mainSort.querySelector('.trip-events__list');
 
+// GET THE DATA
 const url = 'https://16.ecmascript.pages.academy/big-trip/';
 const pointsUrl = `${url}points`;
 const fetchOptions = {
@@ -32,17 +33,16 @@ const fetchOptions = {
     'Authorization': `Basic ${getRandomString()}`,
   },
 };
-
 const cb = (events) => {
-  renderTemplate(itemsList, siteCreatePointTemplate(events[0]), RenderPosition.BEFOREEND);
   console.log(events);
+
+  renderTemplate(itemsList, siteCreatePointTemplate(events[0]), RenderPosition.BEFOREEND);
+  for (let i = 1; i < events.length; i++) {
+    renderTemplate(itemsList, sitePointTemplate(events[i]), RenderPosition.BEFOREEND);
+  }
 };
 
 fetch(pointsUrl, fetchOptions)
   .then((response) => response.json())
   .then((events) => cb(events));
 
-
-// for (let i = 1; i < POINTS_COUNT; i++) {
-//   renderTemplate(itemsList, sitePointTemplate(events[i]), RenderPosition.BEFOREEND);
-// }
--- src/view/site-list-view.js
+++ src/view/site-list-view.js
@@ -1,4 +1,26 @@
-export const siteCreatePointListTemplate = () => (
+import { createElement } from '../render';
+
+const createSitePointListTemplate = () => (
   `<ul class="trip-events__list">
   </ul>`
 );
+
+export default class SitePointListView {
+  #element = null;
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    return createSitePointListTemplate();
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
