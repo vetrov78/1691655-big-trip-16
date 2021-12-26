8ca2de519563a5b8e9d8175e92a6852a373e8e66
Andrei Vetrov <vettrov78@gmail.com>
2021-12-26 23:46:01 +0300

    Перенес отрисовку событий в презентер

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,9 +1,16 @@
-import { render, RenderPosition } from '../utils/render';
+import { RenderPosition, render, replace } from '../utils/render';
+import { isEscPressed } from '../utils/utils';
+import SiteEmptyView from '../view/site-empty/site-empty-view';
+import PointsListView from '../view/site-list/site-list-view';
 import SiteSortView from '../view/site-sort/site-sort-view';
+import SitePointView from '../view/site-points/site-point-view';
+import EditPointView from '../view/site-edit/site-edit-view';
 
 export default class BoardPresenter {
   #boardContainer = null;
-  #boardComponent = new SiteSortView();
+  #sortComponent = new SiteSortView();
+  #invitationComponent = new SiteEmptyView();
+  #pointsListComponent = new PointsListView();
 
   #boardEvents = [];
 
@@ -14,6 +21,71 @@ export default class BoardPresenter {
   init = (boardEvents) => {
     this.#boardEvents = [...boardEvents];
 
-    render(this.#boardContainer, this.#boardComponent, RenderPosition.BEFOREEND );
+    this.#renderSort();
+    this.#renderBoard();
+  }
+
+  #renderSort = () => {
+    render(this.#boardContainer, this.#sortComponent, RenderPosition.BEFOREEND);
+  }
+
+  #renderInvitation = () => {
+    render(this.#boardContainer, this.#invitationComponent, RenderPosition.BEFOREEND);
+  }
+
+  #renderPointsList = () => {
+    render(this.#boardContainer, this.#pointsListComponent, RenderPosition.BEFOREEND);
+  }
+
+  #renderPoints = () => {
+    this.#boardEvents.forEach(
+      (event) => {
+        const pointComponent = new SitePointView(event);
+        const pointEditComponent = new EditPointView(event);
+
+        const replacePointToEditPoint = () => {
+          replace(pointEditComponent, pointComponent);
+        };
+
+        const replaceEditPointToPoint = () => {
+          replace(pointComponent, pointEditComponent);
+        };
+
+        const onEscKeyDown = (evt) => {
+          if (isEscPressed(evt)) {
+            evt.preventDefault();
+            replaceEditPointToPoint();
+            document.removeEventListener('keydown', onEscKeyDown);
+          }
+        };
+
+        pointComponent.setOpenEditHandler (() => {
+          replacePointToEditPoint();
+          document.addEventListener('keydown', onEscKeyDown);
+        });
+
+        pointEditComponent.setCloseClickHandler (() => {
+          replaceEditPointToPoint();
+          document.removeEventListener('keydown', onEscKeyDown);
+        });
+
+        pointEditComponent.setFormSubmitHandler(() => {
+          replaceEditPointToPoint();
+          document.removeEventListener('keydown', onEscKeyDown);
+        });
+
+        render(this.#pointsListComponent, pointComponent, RenderPosition.BEFOREEND);
+      }
+    );
+  }
+
+  #renderBoard = () => {
+    if (this.#boardEvents.length === 0) {
+      this.#renderInvitation();
+      return;
+    }
+
+    this.#renderPointsList();
+    this.#renderPoints();
   }
 }
--- src/utils/render.js
+++ src/utils/render.js
@@ -33,3 +33,21 @@ export const createElement = (template) => {
 
   return newElement.firstChild;
 };
+
+//Скопировал из проекта taskmanager
+export const replace = (newElement, oldElement) => {
+  if (newElement === null || oldElement === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  const newChild = newElement instanceof AbstractView ? newElement.element : newElement;
+  const oldChild = oldElement instanceof AbstractView ? oldElement.element : oldElement;
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null) {
+    throw new Error('Parent element doesn\'t exist');
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
